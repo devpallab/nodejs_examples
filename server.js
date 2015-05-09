bf9844310e62da69ee7eb024db04bd6b9038b9b7
unknown <pallab.pal@LMKOLPC058.learningmatekol.local>
2015-05-09 17:01:52 +0530

    Files updated

---
+++ server.js
@@ -0,0 +1,10 @@
+// creating a server
+
+
+var http = require("http");
+
+http.createServer(function(request,response){
+	response.writeHead(200, {"Content-Type": "text/plain"});
+	response.write("Hello World");
+	response.end();
+}).listen(2000);
